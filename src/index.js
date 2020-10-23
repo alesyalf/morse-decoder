f5f0e19f8d7a235627a67cdd760f98cb8f00181a
Alesya Demeshko <demeshko.ab@phystech.edu>
2020-10-23 23:27:06 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = [];
+    let answer = [];
+    let [i,j] = [0,1];
+    let n = expr.length
+    while (i < n) {
+        let current ='';
+        symbols =(expr.slice(i,(+(j+'0'))));
+        for (let i = 0; i <= 10; i = i + 2) {
+            if(symbols[i] === '*') {
+                current = ` `;
+                break;
+            }
+            if(symbols[i] + symbols[i+1] === '00') {
+                current += '';
+                continue;
+            }
+            if(symbols[i] + symbols[i+1] === '10') {
+                current += '.';
+                continue;
+            }
+            if(symbols[i] + symbols[i+1] === '11') {
+                current += '-';
+                continue;
+            }
+        }
+        current === ` ` ? answer.push(` `) : answer.push(MORSE_TABLE[current]);
+        i = i + 10;
+        j++;
+    }
+    answer = answer.join('');
+    return answer;
 }
 
 module.exports = {
